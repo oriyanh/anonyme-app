719e94262cf1561108499b1dca43e0b7a8392850
Amit Segal <segalamit101@gmail.com>
2019-09-10 14:39:44 +0300

    Working camera preview

--- anonyME/app/src/main/java/com/mashehu/anonyme/MainActivity.java
+++ anonyME/app/src/main/java/com/mashehu/anonyme/MainActivity.java
@@ -15,9 +15,31 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Python py = Python.getInstance();
-        PyObject res = py.getModule("AdvBox.applications.face_recognition_attack.facenet_fr").
-                        callAttr("main", Constants.FILES_PATH, Constants.CACHE_PATH);
-        Log.d("ANONYME", "result file path = " + res.toString());
+//        Python py = Python.getInstance();
+//        PyObject res = py.getModule("AdvBox.applications.face_recognition_attack.facenet_fr").
+//                        callAttr("main", Constants.FILES_PATH, Constants.CACHE_PATH);
+//        Log.d("ANONYME", "result file path = " + res.toString());
     }
+
+//    public static File getOutputDirectory(Context context)
+//    {
+//        Context applicationContext = context.getApplicationContext();
+//        File mediaDir;
+//        if (context.getExternalMediaDirs().length != 0)
+//        {
+//            mediaDir = context.getExternalMediaDirs()[0];
+//        }
+//        else
+//        {
+//            mediaDir = null;
+//        }
+//        File outputDir = new File(mediaDir,
+//                applicationContext.getResources().getString(R.string.app_name));
+//        outputDir.mkdirs();
+//        if ((mediaDir != null) && (mediaDir.exists()))
+//        {
+//            return mediaDir;
+//        }
+//        return applicationContext.getFilesDir();
+//    }
 }
--- anonyME/app/src/main/java/com/mashehu/anonyme/common/Constants.java
+++ anonyME/app/src/main/java/com/mashehu/anonyme/common/Constants.java
@@ -1,7 +1,7 @@
 package com.mashehu.anonyme.common;
 
 final public class Constants {
-
+	public static int CAMERA_PERMISSION_REQUEST_CODE = 1993;
 	public static String CACHE_PATH;
 	public static String FILES_PATH;
 }
