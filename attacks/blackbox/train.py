0965233df2955c284bb983d7e922e559736c7193
Oriyan Hermoni <oriyanh@cs.huji.ac.il>
2020-03-09 16:39:35 +0200

    Refactored training and added main()

--- attacks/blackbox/train.py
+++ attacks/blackbox/train.py
@@ -3,7 +3,6 @@ from PIL import Image
 from matplotlib import pyplot as plt
 
 import numpy as np
-import tensorflow as tf
 from keras_vggface import utils
 from mtcnn.mtcnn import MTCNN
 from attacks.blackbox.substitute_model import SubstituteModel
@@ -14,7 +13,6 @@ from keras_vggface.vggface import VGGFace
 import attacks.blackbox.substitute_model as substitute
 
 GPU_CONST = '2'
-
 
 def extract_face(filename, required_size=(224, 224)):
     # load image from file
@@ -32,36 +30,43 @@ def extract_face(filename, required_size=(224, 224)):
     image = Image.fromarray(face)
     image = image.resize(required_size)
     face_array = np.asarray(image)
-    return face_array.astype('float32')
+    return face_array
 
 def init_black_box_vggface2_keras():
     return VGGFace(model='resnet50')
 
 
 
-def train(oracle, epochs_substitute, epochs_training, batch_size):
+def train(oracle, num_oracle_classes, nepochs_substitute, nepochs_training, batch_size):
+    assert nepochs_substitute > 0
     x_train, y_train = params.load_initial_set(params.NUM_INIT_SAMPLES)
-
-    for epoch in range(epochs_substitute):
-        substitute_model = SubstituteModel()
-        estimated_labels = oracle(x_train)
-        substitute.train(substitute_model, x_train, estimated_labels, epochs_training, batch_size)
+    for epoch in range(nepochs_substitute):
+        substitute_model = SubstituteModel(num_oracle_classes)
+        estimated_labels = oracle.predict(x_train)
+        substitute.train(substitute_model, x_train, estimated_labels, nepochs_training, batch_size)
         x_train = augment_dataset(oracle, x_train, estimated_labels)
 
-    # TODO save weights
+    substitute.save_model(substitute_model, params.SUBSTITUTE_WEIGHTS_PATH)
+    return substitute_model
 
-    return substitute
+LOAD_WEIGHTS = False
+
+def main():
+    oracle = init_black_box_vggface2_keras()
+    if LOAD_WEIGHTS:
+        substitute_model = substitute.load_model(params.SUBSTITUTE_WEIGHTS_PATH, params.NUM_CLASSES_VGGFACE)
+    else:
+        substitute_model = train(oracle, params.NUM_CLASSES_VGGFACE,params.EPOCHS_SUBSTITUTE, params.EPOCHS_TRAINING, params.BATCH_SIZE)
+
 
 if __name__ == '__main__':
     model = init_black_box_vggface2_keras()
 
-    crop_size = (224, 224, 3)
 
-    # img = extract_face('/cs/ep/503/vggface2/vggface2_test/test/n000001/0001_01.jpg')
-    img = extract_face('channing_tatum.jpg')
+    img = extract_face('/cs/ep/503/oriyan/repo/channing_tatum.jpg')
     plt.imshow(img)
     plt.show()
-    x =img[np.newaxis, ...]
+    x =img[np.newaxis, ...].astype(np.float)
 
     x = utils.preprocess_input(x, version=2)
     preds = model.predict(x)
