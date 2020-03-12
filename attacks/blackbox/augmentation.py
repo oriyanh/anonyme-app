289d10257a3e0ce209ed3ea80aa3b241cbbf4a07
Oriyan Hermoni <oriyanh@cs.huji.ac.il>
2020-03-12 17:22:03 +0200

    Optimized dataset augmentation for lighter resource utilization

--- attacks/blackbox/augmentation.py
+++ attacks/blackbox/augmentation.py
@@ -1,17 +1,35 @@
 import tensorflow as tf
+import numpy as np
 
 
 BOUNDS = (0, 1)
+BATCH_SIZE = 32
 
-def augment_dataset(oracle, input_images, scale):
-    images_tensor = tf.constant(input_images, dtype=tf.float32)
-    predictions = oracle(images_tensor)
+def augment_dataset(model, input_images, scale):
+    nbatches = (input_images.shape[0] // BATCH_SIZE) + 1
+    augmented_dataset = None
+    for b in range(nbatches):
+        last_index = np.min(((b + 1) * BATCH_SIZE, input_images.shape[0]))
+        batch = input_images[b * BATCH_SIZE:last_index]
+        augmented_batch = augment_batch(model, batch, scale)
+        augmented_dataset = np.concatenate((augmented_dataset, augmented_batch),
+                                      axis=0) if augmented_dataset is not None else augmented_batch
+        print(f"Augmentation progress: {100 * (b + 1) / nbatches:.2f}%")
+
+    # augmented_dataset = tf.clip_by_value(augmented_dataset, *BOUNDS)
+    # with tf.Session() as sess:
+    #     sess.run(tf.global_variables_initializer())
+    #     new_dataset = sess.run(augmented_dataset)
+    np.random.shuffle(augmented_dataset)
+    return augmented_dataset
+
+def augment_batch(model, batch, scale):
+    images_tensor = tf.Variable(batch, dtype=tf.float64)
+    predictions = model(images_tensor)
     jacobian = tf.gradients(predictions, images_tensor)
-    augmented_images = images_tensor + scale * tf.sign(jacobian)
-    augmented_dataset = tf.random.shuffle(tf.concat((input_images, augmented_images[0]), axis=0))
-
-    augmented_dataset = tf.clip_by_value(augmented_dataset, *BOUNDS)
+    augmented_batch = batch + scale * tf.sign(jacobian)
+    augmented_batch = tf.clip_by_value(augmented_batch[0], *BOUNDS)
     with tf.Session() as sess:
         sess.run(tf.global_variables_initializer())
-        new_dataset=sess.run(augmented_dataset)
-    return new_dataset
+        augmented_batch_np = sess.run(augmented_batch)
+    return augmented_batch_np
