fa9d841610fb870862bf322a2a439154f239ea0b
oriyanh <oriyanh@cs.huji.ac.il>
2020-03-16 17:25:08 +0200

    Modified alternative training function to use alternative dataset augmentation and alternative model construction + training functions. UNTESTED

--- attacks/blackbox/train.py
+++ attacks/blackbox/train.py
@@ -8,10 +8,9 @@ from keras_vggface import utils
 import attacks.blackbox.params as params
 import attacks.blackbox.substitute_model as substitute
 import attacks.blackbox.utilities
-from attacks.blackbox.substitute_model import SubstituteModel
+from attacks.blackbox.substitute_model import SubstituteModel, SubstituteModel2
 from attacks.blackbox.blackbox_model import get_vggface_model
-from attacks.blackbox.utilities import extract_face
-from attacks.blackbox.augmentation import augment_dataset
+from attacks.blackbox.augmentation import augment_dataset, augment_dataset2
 
 
 def train(oracle, num_oracle_classes, nepochs_substitute, nepochs_training, batch_size):
@@ -20,7 +19,7 @@ def train(oracle, num_oracle_classes, nepochs_substitute, nepochs_training, batc
     x_train = np.asarray(x_train).astype(np.float)
     x_train = utils.preprocess_input(x_train, version=2)
     for epoch in range(nepochs_substitute):
-        print(f"Starting training on new substitute model, epoch #{epoch+1}")
+        print(f"Starting training on new substitute model, epoch #{epoch + 1}")
         model = SubstituteModel(num_oracle_classes)
         y_train = predict_in_batches(oracle, x_train, 32)
         substitute.train(model, x_train, y_train, nepochs_training, batch_size)
@@ -38,13 +37,14 @@ def train(oracle, num_oracle_classes, nepochs_substitute, nepochs_training, batc
     return model
 
 def train2(oracle, num_oracle_classes, nepochs_substitute, nepochs_training, batch_size):
-    assert nepochs_substitute > 0
-    datagen = tf.keras.preprocessing.image.ImageDataGenerator()
+    assert nepochs_substitute > 0 and nepochs_training > 0
+    train_dir = params.TRAINING_SET_ALIGNED_PATH
+    validation_dir = train_dir
     for epoch in range(nepochs_substitute):
-        print(f"Starting training on new substitute model, epoch #{epoch+1}")
-        model = SubstituteModel(num_oracle_classes)
-        substitute.train2(model, oracle, datagen, train_dir, validation_dir, nepochs_substitute, batch_size, nsteps)
-        x_train = augment_dataset(model, x_train, params.LAMBDA)
+        print(f"Starting training on new substitute model, epoch #{epoch + 1}")
+        model = SubstituteModel2(num_oracle_classes)
+        substitute.train2(model, oracle, train_dir, validation_dir, nepochs_training, batch_size)
+        train_dir = augment_dataset2(model, train_dir, params.LAMBDA)
     substitute.save_model(model, params.SUBSTITUTE_WEIGHTS_PATH)
     return model
 
@@ -66,8 +66,10 @@ def main():
     if LOAD_WEIGHTS:
         substitute_model = substitute.load_model(params.SUBSTITUTE_WEIGHTS_PATH, params.NUM_CLASSES_VGGFACE)
     else:
-        substitute_model = train(oracle, params.NUM_CLASSES_VGGFACE, params.EPOCHS_SUBSTITUTE,
-                                 params.EPOCHS_TRAINING, params.BATCH_SIZE)
+        # substitute_model = train(oracle, params.NUM_CLASSES_VGGFACE, params.EPOCHS_SUBSTITUTE,
+        #                          params.EPOCHS_TRAINING, params.BATCH_SIZE)
+        substitute_model = train2(oracle, params.NUM_CLASSES_VGGFACE, params.EPOCHS_SUBSTITUTE,
+                                  params.EPOCHS_TRAINING, params.BATCH_SIZE)
 
 if __name__ == '__main__':
     main()
