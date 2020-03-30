e35b557ac99300745a12331e52b3712763f47f1b
Oriyan Hermoni <oriyanh@cs.huji.ac.il>
2020-03-30 18:19:37 +0300

    Updated training script with higher resources

--- scripts/sbatch_train.sh
+++ scripts/sbatch_train.sh
@@ -4,7 +4,7 @@
 #SBATCH --cpus-per-task=4
 #SBATCH --output=/cs/ep/503/outputs/slurm_train.out
 #SBATCH --err=/cs/ep/503/outputs/slurm_train.err
-#SBATCH --mem-per-cpu=4000M
+#SBATCH --mem-per-cpu=8000M
 #SBATCH --time=1-0
 #SBATCH --account=peleg
 #SBATCH --constraint="lucy"
@@ -15,5 +15,8 @@ setenv PYTHONPATH $PYTHONPATH\:/cs/ep/503/oriyan/repo
 source /cs/ep/503/venv/bin/activate.csh
 module load tensorflow
 python3 /cs/ep/503/oriyan/repo/attacks/blackbox/train.py
+echo "Finished training, starting augmentation"
+python3 /cs/ep/503/oriyan/repo/attacks/blackbox/augmentation.py
+echo "Done"
 
 
