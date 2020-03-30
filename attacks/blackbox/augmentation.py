7567a0aba13fa81086cb4b56b5dcee1f628064b6
Oriyan Hermoni <oriyanh@cs.huji.ac.il>
2020-03-30 18:20:44 +0300

    Cleanup

--- attacks/blackbox/augmentation.py
+++ attacks/blackbox/augmentation.py
@@ -12,72 +12,70 @@ from keras_preprocessing.image import ImageDataGenerator
 BOUNDS = (0, 255)
 BATCH_SIZE = 32
 
-def augment_dataset(model, input_images, scale):
-    nbatches = (input_images.shape[0] // BATCH_SIZE) + 1
-    augmented_dataset = None
-    for b in range(nbatches):
-        last_index = np.min(((b + 1) * BATCH_SIZE, input_images.shape[0]))
-        batch = input_images[b * BATCH_SIZE:last_index]
-        augmented_batch = augment_batch(model, batch, scale)
-        augmented_dataset = np.concatenate((augmented_dataset, augmented_batch),
-                                           axis=0) if augmented_dataset is not None else augmented_batch
-        print(f"Augmentation progress: {100 * (b + 1) / nbatches:.2f}%")
+sess = tf.Session()
+with sess.as_default():
+    sess.run(tf.global_variables_initializer())
 
-    # augmented_dataset = tf.clip_by_value(augmented_dataset, *BOUNDS)
-    # with tf.Session() as sess:
-    #     sess.run(tf.global_variables_initializer())
-    #     new_dataset = sess.run(augmented_dataset)
-    np.random.shuffle(augmented_dataset)
-    return augmented_dataset
+def augment_dataset(model, image_dir, scale):
+    batch_size = 16
+    datagen = tf.keras.preprocessing.image.ImageDataGenerator()
 
-def augment_batch(model, batch, scale):
-    images_tensor = tf.Variable(batch, dtype=tf.float64)
-    predictions = model(images_tensor)
-    jacobian = tf.gradients(predictions, images_tensor)
-    augmented_batch = batch + scale * tf.sign(jacobian)
-    augmented_batch = tf.clip_by_value(augmented_batch[0], *BOUNDS)
-    with tf.Session() as sess:
-        sess.run(tf.global_variables_initializer())
-        augmented_batch_np = sess.run(augmented_batch)
-    return augmented_batch_np
-
-def augment_dataset2(model, image_dir, scale):
-    datagen = ImageDataGenerator()
-    image_it = datagen.flow_from_directory(image_dir, class_mode=None,
-                                           batch_size=32,
+    image_it = datagen.flow_from_directory(image_dir, class_mode=None, batch_size=batch_size,
                                            shuffle=False, target_size=(224, 224))
-    nimages = len(image_it.filepaths)
-    nbatches = (nimages // BATCH_SIZE) + 1
+    files = [os.path.join(image_dir, name) for name in image_it.filenames]
+    nimages = image_it.n
+    nbatches = (nimages // batch_size) + 1
     new_image_dir = os.path.join(params.DATASET_BASE_PATH, "intermediate_images",
-                                 datetime.now().strftime("%H%M%S%f"))
+                                 datetime.now().strftime("%Y%m%d%H%M%S%f"))
     os.makedirs(new_image_dir, mode=0o777)
     fname = 1
-    for b in range(nbatches):
-        batch = image_it.next()
-        augmented_batch = augment_batch2(model, batch, scale)
-        for im_array in augmented_batch:
-            im = Image.fromarray(im_array)
-            im.save(os.path.join(new_image_dir,
-                                 f"{fname}.jpg".rjust(11, "0")))
-            fname += 1
-        print(f"Augmentation progress: {100 * (b + 1) / nbatches:.2f}%")
+    step = 0
+    load_img_fn = lambda x: np.asarray(Image.open(x)).astype(np.float32)
+    save_img_fn = lambda x, y: Image.fromarray(x).save(os.path.join(new_image_dir,
+                                                                    f"{y}.jpg".rjust(11, "0")))
+    for nbatch in range(nbatches):
+        if step >= nbatches:
+            break
 
+        last_index = min(((nbatch + 1) * batch_size, nimages))
+        file_batch = files[nbatch * batch_size:last_index]
+        batch = np.asarray([load_img_fn(f) for f in file_batch])
+        batch_norm = utils.preprocess_input(batch, version=2)
+        diff = batch - batch_norm
+        fnames = np.arange(fname, batch.shape[0] * 2).astype(np.uint32)
+        batch_ph = tf.placeholder(tf.float32, shape=[None, 224, 224, 3], name="batch_in")
+        # fnames_ph = tf.placeholder(tf.uint32, shape=[None], name="fnames")
+        augmented_batch_tensor = preprocess(model, batch_ph, scale, diff)
+        # fwrite = save_batch(augmented_batch_tensor, fnames_ph)
+        with sess.as_default():
+            augmented_batch = sess.run(augmented_batch_tensor, feed_dict={batch_ph: batch_norm})
+        for im_orig, im_augmented in zip(batch.astype(np.uint8), augmented_batch):
+            save_img_fn(im_orig, fname)
+            save_img_fn(im_augmented, fname + 1)
+            fname += 2
+        fname += fnames.shape[0]
+        step += 1
+        print(
+            f"Augmentation progress: augmented {step * batch_size * 2} / {nbatches * batch_size * 2} images ({100 * step / nbatches:.2f}%)")
+    sess.close()
     return new_image_dir
 
-def augment_batch2(model, batch, scale):
+@tf.function
+def preprocess(model, batch, scale, diff):
     with tf.GradientTape(persistent=True) as tape:
         tape.watch(batch)
-        predictions = model(batch)
-        pred = tf.argmax(predictions, axis=1)
-    jacobian = tape.batch_jacobian(pred, batch)
-    augmented_batch = batch + scale * tf.sign(jacobian)
-    augmented_batch = tf.clip_by_value(augmented_batch[0], *BOUNDS)
-    with tf.Session() as sess:
-        sess.run(tf.global_variables_initializer())
-        augmented_batch_np = sess.run(augmented_batch)
-    return augmented_batch_np
+        label_batch = model(batch)
+    jacobian = tape.gradient(label_batch, batch)
+    augmented_batch = tf.scalar_mul(scale, tf.sign(jacobian, name="SignJacobian"), name="ScaleJacobian")
+    augmented_batch = tf.add(batch, augmented_batch, name="AddScaledJacobian")
+    # augmented_batch = batch + scale * tf.sign(jacobian, name="SignJacobian")
+    augmented_batch = tf.add(augmented_batch, diff, name="Decenter")
+    # augmented_batch += diff
+    augmented_batch = tf.clip_by_value(augmented_batch, *BOUNDS, name="Clip")
+    augmented_batch = tf.cast(augmented_batch, tf.uint8, "Cast")
+    return augmented_batch
 
-def augment_dataset3(model, image_dir, scale):
+def augment_dataset_alt(model, image_dir, scale):
     batch_size = 32
     # image_ds, nimages = get_augmentation_dataset(model, image_dir, scale, batch_size)
     datagen = tf.keras.preprocessing.image.ImageDataGenerator()
@@ -117,9 +115,9 @@ def augment_dataset3(model, image_dir, scale):
         #     break
         augmented_batch = preprocess(model, batch, scale, diff)
         fnames = np.arange(fname, fname + batch.shape[0])
-        fwrite = save_batch(augmented_batch, fnames)
-        with sess.as_default():
-            sess.run(fwrite)
+        # fwrite = save_batch(augmented_batch, fnames)
+        # with sess.as_default():
+        #     sess.run(fwrite)
         fname += batch.shape[0]
         # for im, im_augmented in zip(batch, augmented_batch):
         #     save_img_fn(im, fname)
@@ -137,82 +135,6 @@ def augment_dataset3(model, image_dir, scale):
             f"Augmentation progress: augmented {step * batch_size * 2} / {nbatches * batch_size * 2} images ({100 * step / nbatches:.2f}%)")
 
     return new_image_dir
-
-def augment_dataset4(model, image_dir, scale):
-    batch_size = 32
-    datagen = tf.keras.preprocessing.image.ImageDataGenerator()
-
-    image_it = datagen.flow_from_directory(image_dir, class_mode=None, batch_size=batch_size,
-                                           shuffle=False, target_size=(224, 224))
-    files = [os.path.join(image_dir, name) for name in image_it.filenames]
-    nimages = image_it.n
-    nbatches = (nimages // BATCH_SIZE) + 1
-    new_image_dir = os.path.join(params.DATASET_BASE_PATH, "intermediate_images",
-                                 datetime.now().strftime("%Y%m%d%H%M%S%f"))
-    os.makedirs(new_image_dir, mode=0o777)
-    fname = 1
-    step = 0
-    load_img_fn = lambda x: np.asarray(Image.open(x)).astype(np.float32)
-    save_img_fn = lambda x, y: Image.fromarray(x).save(os.path.join(new_image_dir,
-                                                                    f"{y}.jpg".rjust(11, "0")))
-    # sess = tf.Session()
-    # with sess.as_default():
-    #     sess.run(tf.global_variables_initializer())
-    for nbatch in range(nbatches):
-        if step >= nbatches:
-            break
-
-        last_index = min(((nbatch + 1) * batch_size, nimages))
-        file_batch = files[nbatch * batch_size:last_index]
-        batch = np.asarray([load_img_fn(f) for f in file_batch])
-        batch_norm = utils.preprocess_input(batch, version=2)
-        diff = batch - batch_norm
-        fnames = np.arange(fname, batch.shape[0]*2).astype(np.uint32)
-        batch_ph = tf.placeholder(tf.float32, shape=[None, 224, 224, 3], name="batch_in")
-        # fnames_ph = tf.placeholder(tf.uint32, shape=[None], name="fnames")
-        augmented_batch_tensor = preprocess(model, batch_ph, scale, diff)
-        # fwrite = save_batch(augmented_batch_tensor, fnames_ph)
-        with sess.as_default():
-            augmented_batch = sess.run(augmented_batch_tensor, feed_dict={batch_ph: batch_norm})
-            # sess.run(fwrite, feed_dict={batch_ph: batch_norm, fnames_ph: fnames})
-        for im_orig, im_augmented in zip(batch.astype(np.uint8), augmented_batch):
-            save_img_fn(im_orig, fname)
-            save_img_fn(im_augmented, fname + 1)
-            fname += 2
-        fname += fnames.shape[0]
-        step += 1
-        print(
-            f"Augmentation progress: augmented {step * batch_size * 2} / {nbatches * batch_size * 2} images ({100 * step / nbatches:.2f}%)")
-    sess.close()
-    return new_image_dir
-
-@tf.function
-def preprocess(model, batch, scale, diff):
-    with tf.GradientTape(persistent=True) as tape:
-        tape.watch(batch)
-        label_batch = model(batch)
-    jacobian = tape.gradient(label_batch, batch)
-    augmented_batch = tf.scalar_mul(scale, tf.sign(jacobian, name="SignJacobian"), name="ScaleJacobian")
-    augmented_batch = tf.add(batch, augmented_batch, name="AddScaledJacobian")
-    # augmented_batch = batch + scale * tf.sign(jacobian, name="SignJacobian")
-    augmented_batch = tf.add(augmented_batch, diff, name="Decenter")
-    # augmented_batch += diff
-    augmented_batch = tf.clip_by_value(augmented_batch, *BOUNDS, name="Clip")
-    augmented_batch = tf.cast(augmented_batch, tf.uint8, "Cast")
-    return augmented_batch
-
-# @tf.function(input_signature=[tf.TensorSpec(shape=None, dtype=tf.uint8), tf.TensorSpec(shape=None, dtype=tf.uint32)])
-def save_batch(batch, fnames):
-    elements = batch, fnames
-    fwrite = tf.map_fn(lambda x: save_image(x[0], x[1]), elements, dtype=tf.Operation, name="WrFiles")
-    return fwrite
-
-# @tf.function
-def save_image(image, fname):
-    image_encoded = tf.image.encode_jpeg(image, quality=100, name="Encode")
-    fname_fmt = tf.strings.format("{}.jpg", fname, name="alcFileName")
-    fwrite = tf.io.write_file(fname_fmt, image_encoded, name="WrFile")
-    return fwrite
 
 def get_augmentation_dataset(model, image_dir, scale, batch_size):
     datagen = tf.keras.preprocessing.image.ImageDataGenerator()
@@ -246,16 +168,14 @@ def get_augmentation_dataset(model, image_dir, scale, batch_size):
                                                output_types=(tf.uint8))
     return ds_images, len(image_it.filepaths)
 
-sess = tf.Session()
-with sess.as_default():
-    sess.run(tf.global_variables_initializer())
 if __name__ == '__main__':
     from attacks.blackbox.substitute_model import load_model
+
 
     tf.keras.backend.set_session(sess)
     substitute_model = load_model(params.SUBSTITUTE_WEIGHTS_PATH, params.NUM_CLASSES_VGGFACE)
     init_b = np.random.randn(1, 224, 224, 3)
     assert substitute_model.predict(init_b) is not None
     print(f"Successfully loaded model")
-    augment_dataset4(substitute_model, params.TRAINING_SET_ALIGNED_PATH, params.LAMBDA)
+    augment_dataset(substitute_model, params.TRAINING_SET_ALIGNED_PATH, params.LAMBDA)
     sess.close()
--- attacks/blackbox/substitute_model.py
+++ attacks/blackbox/substitute_model.py
@@ -1,42 +1,18 @@
 import tensorflow as tf
 from keras_vggface import utils
-from tensorflow.keras import Model
 from tensorflow.keras.layers import Dense, Conv2D, MaxPool2D, Flatten
 
 import attacks.blackbox.params as params
 import numpy as np
 from attacks.blackbox.blackbox_model import graph, sess
 
+
 loss_obj = tf.keras.losses.SparseCategoricalCrossentropy()
 optimizer = tf.keras.optimizers.Adam(params.LEARNING_RATE, beta_1=params.MOMENTUM)
 train_loss = tf.keras.metrics.Mean(name='train_loss')
 train_accuracy = tf.keras.metrics.SparseCategoricalAccuracy(name='train_accuracy')
 
-class SubstituteModel(Model):
-
-    def __init__(self, num_classes):
-        super(SubstituteModel, self).__init__()
-        self.conv1 = Conv2D(64, 2)
-        self.maxpool1 = MaxPool2D(2)
-        self.conv2 = Conv2D(64, 2)
-        self.maxpool2 = MaxPool2D(2)
-        self.flatten = Flatten()
-        self.dense1 = Dense(200, activation='sigmoid')
-        self.dense2 = Dense(200, activation='sigmoid')
-        self.dense2 = Dense(100, activation='relu')
-        self.dense3 = Dense(num_classes, activation='softmax')
-
-    def call(self, x):
-        x = self.conv1(x)
-        x = self.maxpool1(x)
-        x = self.conv2(x)
-        x = self.maxpool2(x)
-        x = self.flatten(x)
-        x = self.dense1(x)
-        x = self.dense2(x)
-        return self.dense3(x)
-
-def SubstituteModel2(num_classes):
+def SubstituteModel(num_classes):
     model = tf.keras.Sequential(layers=[Conv2D(64, 2), MaxPool2D(2), Conv2D(64, 2),
                                         MaxPool2D(2), Flatten(), Dense(200, activation='sigmoid'),
                                         Dense(200, activation='sigmoid'), Dense(100, activation='relu'),
@@ -44,9 +20,8 @@ def SubstituteModel2(num_classes):
     model.compile(optimizer=optimizer, loss='sparse_categorical_crossentropy', metrics=['accuracy'])
     return model
 
-
 def load_model(weights_path, num_classes):
-    model = SubstituteModel2(num_classes)
+    model = SubstituteModel(num_classes)
     model.build(input_shape=[None, 224, 224, 3])
     model.load_weights(weights_path)
     return model
@@ -54,54 +29,7 @@ def load_model(weights_path, num_classes):
 def save_model(model, weights_path):
     model.save_weights(weights_path, save_format='h5')
 
-def get_batches(images, labels, batch_size):
-    nbatches = (images.shape[0] // batch_size) + 1
-    permutation = np.random.permutation(np.arange(images.shape[0]))
-    images_perm = images[permutation]
-    labels_perm = labels[permutation]
-    for b in range(nbatches):
-        last_index = np.min(((b + 1) * batch_size, images.shape[0]))
-        x_train = images_perm[b * batch_size:last_index]
-        y_train = labels_perm[b * batch_size:last_index]
-        yield x_train, y_train
-
-def train(model, images, labels, num_epochs, batch_size):
-    # shuffle_seed = 1000
-
-    # train_ds = tf.data.Dataset.from_tensor_slices(
-    #     (images, labels)).shuffle(shuffle_seed).batch(batch_size)
-
-    train_step = get_train_step()
-    nbatches = images.shape[0] // batch_size
-    for epoch in range(num_epochs):
-        print(f"Start training epoch #{epoch + 1}")
-        nbatch = 0
-        for im_batch, label_batch in get_batches(images, labels, batch_size):
-            nbatch += 1
-            if nbatch > nbatches:
-                break
-            print(f"Training epoch progress: {100 * nbatch / nbatches:.2f}%")
-            train_step(model, im_batch, label_batch)
-        # train_accuracy(labels, model(images))
-        # with tf.Session().as_default():
-        #     print(f'Epoch {epoch + 1}: Loss: {train_loss.result().eval()}, '
-        #           f'Accuracy: {train_accuracy.result().eval() * 100:.2f}%')
-
-        # Reset the metrics for the next epoch
-        # train_loss.reset_states()
-        # train_accuracy.reset_states()
-    # pred = model(images)
-    # res = tf.argmax(pred, axis=1)
-    # with tf.Session() as sess:
-    #     sess.run(tf.global_variables_initializer())
-    #     pred_labels=sess.run(res)
-    #     pred_labels = pred_labels.flatten()
-    # res = labels == pred_labels
-    # accuracy = np.count_nonzero(res) / pred_labels.shape[0]
-    # print(f"Accuracy after {num_epochs} epochs: {accuracy * 100:.2f}%")
-    return model
-
-def train2(model, oracle, train_dir, validation_dir, num_epochs, batch_size):
+def train(model, oracle, train_dir, validation_dir, num_epochs, batch_size):
     # train_gen, nsamples = training_generator(oracle, train_dir, batch_size)
     train_ds, nsamples = get_training_set(oracle, train_dir, batch_size)
     train_step = get_train_step()
@@ -135,6 +63,7 @@ def training_generator(oracle, train_dir, batch_size):
                                            batch_size=batch_size,
                                            shuffle=True, target_size=(224, 224))
     nsteps = (len(train_it.filepaths) // batch_size) + 1
+
     def gen():
         while True:
             # for step in range(nsteps):
@@ -146,13 +75,15 @@ def training_generator(oracle, train_dir, batch_size):
             y_train = np.argmax(label_batch, axis=1)
             yield x_train, y_train
         # print(f"Training epoch progress: {100 * step / nsteps:.2f}%")
+
     return gen, nsteps
+
 def get_training_set(oracle, train_dir, batch_size):
     datagen = tf.keras.preprocessing.image.ImageDataGenerator()
     train_it = datagen.flow_from_directory(train_dir, class_mode=None, batch_size=batch_size,
                                            shuffle=True, target_size=(224, 224))
-    def gen():
 
+    def gen():
         while True:
             # for step in range(nsteps):
             im_batch = train_it.next()
@@ -163,6 +94,7 @@ def get_training_set(oracle, train_dir, batch_size):
             y_train = np.argmax(label_batch, axis=1)
             # print(f"Training epoch progress: {100 * (step+1) / nsteps:.2f}%")
             yield x_train, y_train
+
     ds_images = tf.data.Dataset.from_generator(gen, output_shapes=([None, 224, 224, 3], [None]),
                                                output_types=(tf.float32, tf.int32))
     return ds_images, train_it.n
