0b1738fd6f4ed5c503c71cd564ff83de0ef742b5
Oriyan Hermoni <oriyanh@cs.huji.ac.il>
2020-04-18 18:06:07 +0300

    Added third weights path

--- attacks/blackbox/params.py
+++ attacks/blackbox/params.py
@@ -9,15 +9,18 @@ BATCH_SIZE = 32
 # LAMBDA = 0.1  # Step size for jacobian augmentation (for images in [0,1]
 LAMBDA = 25.6  # Step size for jacobian augmentation (for images in [0, 255]
 NUM_INIT_SAMPLES = 1000
+
 WEIGHTS_DIR = 'weights'
 SQUEEZENET_WEIGHTS_FNAME = 'substitute_squeezenet.h5'
 SQUEEZENET_WEIGHTS_PATH = os.path.join(WEIGHTS_DIR, SQUEEZENET_WEIGHTS_FNAME)
-SUBSTITUTE_WEIGHTS_FNAME = 'substitute_vggface.h5'
-SUBSTITUTE_WEIGHTS_PATH = os.path.join(WEIGHTS_DIR, SUBSTITUTE_WEIGHTS_FNAME)
-# VGGFACE2_BLACKBOX_WEIGHTS_PATH = '/cs/ep/503/vggface2/vggface2_Keras//model/resnet50_softmax_dim512/weights.h5'
+CUSTOM_SUB_WEIGHTS_FNAME = 'substitute_custom.h5'
+CUSTOM_SUB_WEIGHTS_PATH = os.path.join(WEIGHTS_DIR, CUSTOM_SUB_WEIGHTS_FNAME)
+RESNET50_WEIGHTS_FNAME = 'substitute_resnet50.h5'
+RESNET50_WEIGHTS_PATH = os.path.join(WEIGHTS_DIR, RESNET50_WEIGHTS_FNAME)
 FACENET_WEIGHTS_FNAME = 'facenet_model.pb'
 FACENET_WEIGHTS_PATH = os.path.join(WEIGHTS_DIR, FACENET_WEIGHTS_FNAME)
 NUM_CLASSES_VGGFACE = 8631
+
 DATASET_BASE_PATH = '/cs/ep/503/vggface2'  # TODO: hange to os-independent path
 DATASET_UNALIGNED_PATH = os.path.join(DATASET_BASE_PATH, "vggface2_test", "test")
 DATASET_UNALIGNED_TESTLIST = os.path.join(DATASET_BASE_PATH, "test_list.txt")
