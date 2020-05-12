fe5265713aa65ef8146b8405792614e11caaadce
oriyanh <oriyanh@cs.huji.ac.il>
2020-05-12 12:56:34 +0300

    Modified train() function according to new substitute_model.train() function and new models.load_model() function

--- attacks/blackbox/train.py
+++ attacks/blackbox/train.py
@@ -1,33 +1,26 @@
 import os
-
 import numpy as np
-
+import tensorflow as tf
 import attacks.blackbox.params as params
 import attacks.blackbox.substitute_model as substitute
 from attacks.blackbox import models
-from attacks.blackbox.squeezenet import squeeze_net
-from attacks.blackbox.blackbox_model import get_vggface_model, sess
+from attacks.blackbox.models import sess
 from attacks.blackbox.augmentation import augment_dataset
 from project_params import ROOT_DIR
 
 
-def train(oracle, num_oracle_classes, nepochs_substitute, nepochs_training, batch_size):
+def train(oracle, substitute_type, nepochs_substitute, nepochs_training, batch_size):
     assert nepochs_substitute > 0 and nepochs_training > 0
-    train_dir = params.TRAINING_SET_ALIGNED_PATH
+    train_dir = params.TRAIN_SET_ALIGNED
     validation_dir = train_dir
     for epoch in range(1, nepochs_substitute+1):
         print(f"Starting training on new substitute model, epoch #{epoch}")
-        # model = SubstituteModel(num_oracle_classes)
-        # model = squeeze_net(num_oracle_classes)
-        model = models.load_model(model_type='resnet50', trained=False)
-        substitute.train(model, oracle, train_dir, validation_dir, nepochs_training, batch_size)
-        print("Saving")
-        # substitute.save_model(model, params.SUBSTITUTE_WEIGHTS_PATH)
-        # substitute.save_model(model, os.path.join(ROOT_DIR, params.SQUEEZENET_WEIGHTS_PATH))
-        models.save_model(model, 'resnet50')
-        if epoch < nepochs_substitute:
-            print("Augmenting")
-            train_dir = augment_dataset(model, train_dir, params.LAMBDA)
+        model = substitute.train(substitute_type, oracle, train_dir, validation_dir, nepochs_training, batch_size)
+        print("Saving model")
+        models.save_model(model, substitute_type)
+        # if epoch < nepochs_substitute:
+        #     print("Augmenting")
+        #     train_dir = augment_dataset(model, train_dir, params.LAMBDA)
     return model
 
 
@@ -39,10 +32,11 @@ def main():
     if PRE_TRAINED:
         substitute_model = models.load_model(model_type='resnet50', trained=True)
     else:
-        substitute_model = train(oracle, params.NUM_CLASSES_VGGFACE, params.EPOCHS_SUBSTITUTE,
+        substitute_model = train(oracle, 'resnet50', params.EPOCHS_SUBSTITUTE,
                                  params.EPOCHS_TRAINING, params.BATCH_SIZE)
 
 
 if __name__ == '__main__':
+    tf.keras.backend.set_session(sess)
     main()
     sess.close()
