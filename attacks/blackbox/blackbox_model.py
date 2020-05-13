281f4d863ae68cfdfd489ed6537bc8bf010cb1fa
Amit Segal <segalman@cs.huji.ac.il>
2020-05-13 20:39:13 +0300

    Switched unused get_vggface_model method for get_blackbox_prediction which may be expanded when working with remote blackbox classifiers

--- attacks/blackbox/blackbox_model.py
+++ attacks/blackbox/blackbox_model.py
@@ -1,9 +1,20 @@
 import tensorflow as tf
 from keras_vggface import VGGFace
+from keras_vggface.utils import preprocess_input
+from attacks.blackbox.utilities import Singleton
 
 graph = tf.get_default_graph()
 sess = tf.Session(graph=graph)
-def get_vggface_model():
-    tf.keras.backend.set_session(sess)
-    model = VGGFace(model='resnet50')
-    return model
+
+
+class BlackboxModel(metaclass=Singleton):
+    def __init__(self):
+        self.model = VGGFace(model='resnet50')
+
+    def predict(self, input):
+        self.model.predict(preprocess_input(input, version=2))
+
+
+def get_blackbox_prediction(batch):
+    blackbox = BlackboxModel()
+    return blackbox.predict(batch)
--- attacks/blackbox/utilities.py
+++ attacks/blackbox/utilities.py
@@ -49,3 +49,13 @@ def extract_face(mtcnn, pixels, required_size=(224, 224),
     image = image.resize(required_size)
     face_array = np.asarray(image)
     return face_array
+
+
+class Singleton(type):
+    _instances = {}
+
+    def __call__(cls, *args, **kwargs):
+        if cls not in cls._instances:
+            cls._instances[cls] = super(Singleton, cls).__call__(*args,
+                                                                 **kwargs)
+        return cls._instances[cls]
