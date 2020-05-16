2ed70d20cdd115e7855e08479e55c0a839cc3789
oriyanh <oriyanh@cs.huji.ac.il>
2020-05-16 18:51:36 +0300

    Modified to work with images in (0,1) rather than (0,255)

--- attacks/blackbox/augmentation.py
+++ attacks/blackbox/augmentation.py
@@ -5,13 +5,15 @@ from datetime import datetime
 from PIL import Image
 from keras_vggface import VGGFace
 
-from attacks.blackbox.blackbox_model import get_blackbox_prediction
+# from attacks.blackbox.blackbox_model import get_blackbox_prediction
 import attacks.blackbox.params as params
+from attacks.blackbox.models import load_model
+from attacks.blackbox.utilities import sess
 
 
-BOUNDS = (0, 255)
+# BOUNDS = (0, 255)
+BOUNDS = (0., 1.)
 
-sess = tf.Session()
 with sess.as_default():
     sess.run(tf.global_variables_initializer())
 
@@ -30,16 +32,16 @@ def augment_dataset(model, image_dir, scale):
     image_it = datagen.flow_from_directory(
         image_dir, class_mode=None, batch_size=batch_size, shuffle=False,
         target_size=(224, 224))
-    files = [os.path.join(image_dir, name) for name in image_it.filenames]
+    # files = [os.path.join(image_dir, name) for name in image_it.filenames]
     nimages = image_it.n
     nbatches = (nimages // batch_size) + 1
     new_image_dir = os.path.join(
-        params.DATASET_BASE_PATH, "intermediate_images",
-        datetime.now().strftime("%Y%m%d%H%M%S%f"))
+        params.DATASET_BASE_PATH, "intermediate",
+        datetime.now().strftime("%Y%m%d%H%M%S%f"), 'augmented_images')
     os.makedirs(new_image_dir, mode=0o777)
     fname = 1
     step = 0
-    load_img_fn = lambda x: np.asarray(Image.open(x)).astype(np.float32)
+    # load_img_fn = lambda x: np.asarray(Image.open(x)).astype(np.float32)/255.0
     save_img_fn = lambda x, y: Image.fromarray(x).save(
         os.path.join(new_image_dir, f"{y}.jpg".rjust(11, "0")))
 
@@ -53,9 +55,10 @@ def augment_dataset(model, image_dir, scale):
             break
 
         last_index = min(((nbatch + 1) * batch_size, nimages))
-        file_batch = files[nbatch * batch_size:last_index]
-        batch = np.asarray([load_img_fn(f) for f in file_batch])
-        oracle_label_batch = np.argmax(get_blackbox_prediction(batch),
+        # file_batch = files[nbatch * batch_size:last_index]
+        # batch = np.asarray([load_img_fn(f) for f in file_batch])
+        batch = image_it.next().astype(np.float) / 255.
+        oracle_label_batch = np.argmax(model.predict(batch),
                                        axis=1)
         oracle_label_batch = np.vstack(
             [
@@ -74,19 +77,20 @@ def augment_dataset(model, image_dir, scale):
                     oracle_label_batch_ph: oracle_label_batch,
                 })
 
-        for im_orig, im_augmented in zip(batch.astype(np.uint8),
+        for im_orig, im_augmented in zip(batch,
                                          augmented_batch):
-            save_img_fn(im_orig, fname)
-            save_img_fn(im_augmented, fname + 1)
-            fname += 2
+            # save_img_fn(im_orig, fname)
+            img = (im_augmented*255).astype(np.uint8)
+            save_img_fn(img, fname)
+            fname += 1
 
         fname += fnames.shape[0]
         step += 1
         print(f"Augmentation progress: augmented "
-              f"{step * batch_size * 2} / {nbatches * batch_size * 2} images "
+              f"{step * batch_size} / {nimages} images "
               f"({100 * step / nbatches:.2f}%)")
 
-    return new_image_dir
+    return os.path.dirname(new_image_dir)
 
 
 @tf.function
@@ -120,7 +124,8 @@ def augment(model, batch, oracle_label_batch, scale):
                              name="AddScaledJacobians")
     augmented_batch = tf.clip_by_value(augmented_batch, *BOUNDS,
                                        name="Clip")
-    augmented_batch = tf.cast(augmented_batch, tf.uint8, "Cast")
+    # augmented_batch = tf.scalar_mul(augmented_batch, 255., "UpScale")
+    # augmented_batch = tf.cast(augmented_batch, tf.uint8, "Cast")
     return augmented_batch
 
 ### Uncomment function in future if needed ###
@@ -159,7 +164,8 @@ if __name__ == '__main__':
     #                               params.NUM_CLASSES_VGGFACE,
     #                               model='custom')
 
-    substitute_model = VGGFace(model='resnet50', weights=None)
-    augment_dataset(substitute_model, params.TRAINING_SET_ALIGNED_PATH,
+    # substitute_model = VGGFace(model='resnet50', weights=None)
+    model = load_model('resnet50', trained=False, num_classes=25)
+    augment_dataset(model, params.TRAIN_SET_WORKING,
                     params.LAMBDA)
     sess.close()
