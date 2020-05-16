d2f274452d2638488d816afbefcabf4514409b7e
Oriyan Hermoni <oriyanh@cs.huji.ac.il>
2020-05-16 14:07:34 +0300

    Moved `get_training_set` to utilities.py

--- attacks/blackbox/utilities.py
+++ attacks/blackbox/utilities.py
@@ -2,7 +2,9 @@ import os
 import numpy as np
 from PIL import Image
 import tensorflow as tf
+from keras_vggface import utils
 
+from attacks.blackbox.models import graph, sess
 from attacks.blackbox.params import DATASET_TRAIN_LIST, TRAIN_SET_ALIGNED
 
 
@@ -49,3 +51,23 @@ def extract_face(mtcnn, pixels, required_size=(224, 224),
     image = image.resize(required_size)
     face_array = np.asarray(image)
     return face_array
+
+def get_training_set(oracle, train_dir, batch_size):
+    datagen = tf.keras.preprocessing.image.ImageDataGenerator()
+    train_it = datagen.flow_from_directory(train_dir, class_mode=None, batch_size=batch_size,
+                                           shuffle=True, target_size=(224, 224))
+
+    def gen():
+        while True:
+            x_train = train_it.next()
+            im_batch_norm = utils.preprocess_input(x_train, version=2)
+            with graph.as_default():
+                tf.keras.backend.set_session(sess)
+                label_batch = oracle.predict(im_batch_norm)
+            y_train = np.argmax(label_batch, axis=1)
+            yield x_train.astype(np.float32)/255.0, y_train
+
+    ds_images = tf.data.Dataset.from_generator(gen, output_shapes=([None, 224, 224, 3], [None]),
+                                               output_types=(tf.float32, tf.int32))
+    # return ds_images, train_it.n
+    return gen(), train_it.n
