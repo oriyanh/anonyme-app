cc7127b252759e9302776eb19f2bd612fb335a77
Oriyan Hermoni <oriyanh@cs.huji.ac.il>
2020-05-18 19:34:20 +0300

    Added time remaining estimation to train function

--- attacks/blackbox/train.py
+++ attacks/blackbox/train.py
@@ -1,8 +1,10 @@
 import tensorflow as tf
 import attacks.blackbox.params as params
+from time import perf_counter
+from datetime import timedelta
 from attacks.blackbox import models
 from attacks.blackbox.augmentation import augment_dataset
-from attacks.blackbox.utilities import get_training_set, predict_and_save, sess
+from attacks.blackbox.utilities import get_training_set, oracle_classify_and_save, sess
 
 
 def train(oracle, substitute_type, nepochs_substitute, nepochs_training, batch_size):
@@ -15,7 +17,9 @@ def train(oracle, substitute_type, nepochs_substitute, nepochs_training, batch_s
         print(f"Starting training on new substitute model, epoch #{epoch_sub}")
 
         print("a) Acquiring oracle predictions")
-        predict_and_save(oracle, image_dir, params.TRAIN_SET_WORKING, batch_size)
+        # if epoch_sub > 1:
+        #     oracle_classify_and_save(oracle, image_dir, params.TRAIN_SET_WORKING, batch_size)
+        oracle_classify_and_save(oracle, image_dir, params.TRAIN_SET_WORKING, batch_size)
         train_dir = params.TRAIN_SET_WORKING
 
         print("b) Start training substitute model")
@@ -23,8 +27,8 @@ def train(oracle, substitute_type, nepochs_substitute, nepochs_training, batch_s
         model = models.load_model(model_type=substitute_type, trained=False, num_classes=num_classes)
 
         for epoch in range(nepochs_training):
-
             print(f"b.1) Starting training epoch #{epoch + 1}")
+            epoch_start_time = perf_counter()
             epoch_loss = 0.
             epoch_acc = 0.
             step = 0
@@ -34,18 +38,27 @@ def train(oracle, substitute_type, nepochs_substitute, nepochs_training, batch_s
                 [loss, acc] = model.train_on_batch(im_batch, label_batch)
                 epoch_loss += loss
                 epoch_acc += acc
-                print(
-                    f"Step {step + 1}/{nsteps} ({100 * (step + 1) / nsteps:.2f}%) - Loss={loss}, accuracy={acc}")
+                print(f"Step {step + 1}/{nsteps} ({100 * (step + 1) / nsteps:.2f}%) "
+                      f"- Loss={loss}, accuracy={acc}; ", end="")
                 step += 1
+
+                time_now = perf_counter()
+                time_elapsed = time_now - epoch_start_time
+                time_per_step = time_elapsed / (step+1)
+                steps_remaining = nsteps - (step+1)
+                time_remaining = steps_remaining * time_per_step
+                print(f"Est. time remaining for epoch: {timedelta(seconds=time_remaining)}")
+
             epoch_loss /= nsteps
             epoch_acc /= nsteps
             print(f"Average training loss for epoch: {epoch_loss} ; Average accuracy: {epoch_acc}")
             print("b.2) Saving checkpoint")
             models.save_model(model, substitute_type)
+
         if epoch_sub < nepochs_substitute:
             print("c) Augmenting")
-            model = models.load_model(model_type=substitute_type, trained=False, num_classes=num_classes)
             image_dir = augment_dataset(model, train_dir, params.LAMBDA)
+
     return model
 
 PRE_TRAINED = False
