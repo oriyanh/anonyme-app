aa66ad3aa5ac3ebd7b8ac4d34d63a6f8e680160f
Oriyan Hermoni <oriyanh@cs.huji.ac.il>
2020-05-22 15:12:42 +0300

    Fixed training script - but doesn't work with sbatch, only srun

--- scripts/train.sh
+++ scripts/train.sh
@@ -1,21 +1,23 @@
-#!/bin/csh
-
-
-#SBATCH --cpus-per-task=2
+#!/bin/tcsh
+#SBATCH -c2
 #SBATCH --output=/cs/ep/503/outputs/train.out
 #SBATCH --err=/cs/ep/503/outputs/train.err
-#SBATCH --mem=32000M
+#SBATCH --mem=24576M
 #SBATCH --time=2-0
 #SBATCH --account=peleg
-#SBATCH --constraint="lucy"
+#SBATCH --constraint="oxygen"
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=oriyanh
+#SBATCH -vv
+
+echo "Start train script"
 
 umask 2
 
 source /cs/ep/503/venv/bin/activate.csh
 module load tensorflow/1.14.0
 setenv PYTHONPATH $PYTHONPATH\:/cs/ep/503/oriyan/repo
-echo "Start train script"
+
 python3.7 /cs/ep/503/oriyan/repo/attacks/blackbox/train.py
-#echo "Finished training, starting augmentation"
-#python3 /cs/ep/503/oriyan/repo/attacks/blackbox/augmentation.py
+
 echo "Finished train script"
