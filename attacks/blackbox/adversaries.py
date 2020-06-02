cf5484d68f778c0622a7dc7962e51fe527d42956
segalman <amit.segal2@mail.huji.ac.il>
2020-06-03 01:29:34 +0300

    Improved black-box attack performance, attack now works on (0, 1) float32 image batches, and runs until all images have been misclassified/num_iter

--- attacks/blackbox/adversaries.py
+++ attacks/blackbox/adversaries.py
@@ -7,51 +7,85 @@ CLASSIFICATION_LOSS = tf.keras.losses.SparseCategoricalCrossentropy()
 FGSM_ATTACK_NAME = 'fgsm'
 
 
-def generate_adversarial_sample(image, model, attack, kwargs):
-    adversarial_sample = attack(image[np.newaxis, ...], model, **kwargs)
-    return adversarial_sample
+def generate_adversarial_sample(model, im_batch, attack, kwargs, sess=tf.get_default_session()):
+    """
+    Generates adversarial samples for given image batch using given attack and arguments
+    :param model: Model to attack
+    :param im_batch: Batch of images from which to creates adversarial samples
+    :param attack: callable representing adversarial attack
+    :param kwargs: attack-specific keyword arguments
+    :param sess: tensorflow session on which to perform the attack
+    :return: adversarial samples generated from im_batch
+    """
+    im_batch = im_batch.astype(np.float32) / 255.0
+    adv_im_batch = attack(model, im_batch, sess, **kwargs)
+    adv_im_batch = (adv_im_batch * 255.0).astype(np.uint8)
+    return adv_im_batch
 
 
-def run_fgsm_attack(image, model, eps=0.15, num_iter=100):
-    preds = model.predict(image)
-    orig_pred = np.argmax(preds)
-    confidence = preds[0, orig_pred]
-    print(f"Initial predicted class is {orig_pred} with confidence "
-          f"{confidence * 100:.03f}%")
+def calc_preds(model, im_batch):
+    """
+    Calculates class and confidence of predictions
+    :param model:
+    :param im_batch:
+    :return:
+    """
+    batch_preds = model.predict(im_batch)
+    preds = np.argmax(batch_preds, axis=-1)
+    confidence = batch_preds[np.arange(len(preds)), preds]
+
+    print(f"Confidence: \n{np.column_stack((preds, confidence))}")
+
+    return preds
+
+
+def run_fgsm_attack(model, im_batch, sess=tf.get_default_session(),
+                    eps=0.15, num_iter=100):
+    batch_ph = tf.placeholder(tf.float32, shape=[None, 224, 224, 3],
+                              name="batch_in")
+    label_ph = tf.placeholder(tf.int32, shape=[None], name="pred_in")
+    adv_im_batch = fgsm(model, batch_ph, label_ph, eps)
+
+    orig_preds = calc_preds(model, im_batch)
+    preds = orig_preds
+    idx_mask = np.array([True] * len(preds))
 
     for i in range(num_iter):
-        image = fgsm(model, tf.constant(image), tf.constant(orig_pred), eps)
-        cur_preds = model.predict(image)
-        pred = np.argmax(cur_preds)
-        confidence = cur_preds[0, pred]
-        print(f"Iteration {i + 1}/{num_iter}\n"
-              f"Predicted class is {pred} with "
-              f"confidence {confidence * 100:.03f}%")
-        new_pred = np.argmax(model.predict(image))
-        if new_pred != orig_pred:
+        print(f"Iteration {i + 1}/{num_iter}")
+        im_batch[idx_mask] = sess.run(
+            adv_im_batch,
+            feed_dict={
+                batch_ph: im_batch[idx_mask],
+                label_ph: preds[idx_mask],
+            })
+        preds = calc_preds(model, im_batch)
+
+        # Perform adversarial attack on images yet to converge
+        idx_mask = preds == orig_preds
+
+        if not np.any(idx_mask):
             print(f"Convergence reached after {i + 1} iterations")
             break
     else:
         print("Convergence not reached")
-    return image
+    return im_batch
 
 
-def fgsm(model, x, y, eps, bounds=(0., 255.)):
+def fgsm(model, x, y, eps, bounds=(0., 1.)):
     """
     Performs fgsm attack on input x with label y using prediction from given
     model.
     :param model: Model to perform attack on
     :type model: tf.Model
     :param x: Tensor of input image
-    :type x: tf.Tensor
+    :type x: tf.Placeholder
     :param y: Tensor of image label
-    :type y: tf.Tensor
+    :type y: tf.Placeholder
     :param eps: Step size for fgsm attack
     :type eps: float
     :param bounds: 2-Tuple representing image value boundaries
     :type bounds: Tuple[float, float]
     :return: Image after attack iteration
-    :rtype: np.ndarray
     """
 
     (clip_min, clip_max) = bounds
@@ -65,19 +99,39 @@ def fgsm(model, x, y, eps, bounds=(0., 255.)):
 
     adv_x = x + eps * signed_grad
     adv_x = tf.clip_by_value(adv_x, clip_min, clip_max)
-    adv_x = tf.math.floor(adv_x)
 
-    # with sess.as_default():
-    adv_im = tf.get_default_session().run(adv_x)
-
-    return adv_im
+    return adv_x
 
 
-def run_papernot_attack(img, model, label):
+def run_papernot_attack(model, img, sess):
     pass
 
 
 def papernot():
     pass
 
+
 print(f"Successfully loaded module {__file__}")
+
+
+if __name__ == '__main__':
+    from attacks.blackbox.utilities import sess
+    from attacks.blackbox.models import load_model
+    from attacks.blackbox.utilities import get_train_set
+    from attacks.blackbox.params import VALIDATION_SET
+    from matplotlib import pyplot as plt
+
+    train_ds, nsteps_train, num_classes, class_indices = get_train_set(VALIDATION_SET, 4)
+
+    model = load_model('resnet50', num_classes=231, trained=True,
+                       weights_path='/cs/ep/503/dataset/weights/substitute_resnet50_4.h5')
+
+    with sess:
+        for batch, _ in train_ds:
+            adv_batch = generate_adversarial_sample(model, batch, run_fgsm_attack, {}, sess=tf.get_default_session())
+            for i in range(len(batch)):
+                fig, axarr = plt.subplots(1, 2)
+                axarr[0].imshow(batch[i])
+                axarr[1].imshow(batch[i])
+                plt.show()
+            break
